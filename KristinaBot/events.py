065dabc9bd412b39fa709278c901ccbebc11e95a
∂ιρєѕн <83916289+Itzdipesh@users.noreply.github.com>
2021-10-11 19:59:01 +0530

    Update events.py

--- KristinaBot/events.py
+++ KristinaBot/events.py
@@ -13,7 +13,7 @@ from KristinaBot import telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["darkuserbot"]
+db = client["KristinaBot"]
 gbanned = db.gban
 
 def register(**args):
